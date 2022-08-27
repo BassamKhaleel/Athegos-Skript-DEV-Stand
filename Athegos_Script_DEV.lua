deef6a5f3624e8f4b9dc03cb0afbbc0cf07bfb71
BassamKhaleel <cyrgclan@gmail.com>
2022-08-27 13:16:22 +0200

    Auto Updater eigebaut

--- Athegos_Script_DEV.lua
+++ Athegos_Script_DEV.lua
@@ -1,10 +1,38 @@
 util.keep_running()
-util.toast("Athego's Script erfolgreich geladen!")
-coded_for_gtao_version = 1.61
-
 require("natives-1640181023")
 require("natives-1606100775")
 util.require_natives(1627063482)
+util.toast("Athego's Script erfolgreich geladen!")
+coded_for_gtao_version = 1.61
+
+local response = false
+local localVer = 1.00
+async_http.init("raw.githubusercontent.com", "/BassamKhaleel/Athegos-Skript-DEV-Stand/main/AthegosSkriptVersion", function(output)
+    currentVer = tonumber(output)
+    response = true
+    if localVer ~= currentVer then
+        util.toast("Eine neue Version von Athego‘s Skript ist verfügbar, bitte Update das Skript")
+        menu.action(menu.my_root(), "Update Lua", {}, "", function()
+            async_http.init('raw.githubusercontent.com','/BassamKhaleel/Athegos-Skript-DEV-Stand/main/Athegos_Script_DEV.lua',function(a)
+                local err = select(2,load(a))
+                if err then
+                    util.toast("Fehler beim Updaten des Skript‘s. Probiere es später erneut. Sollte der Fehler weiterhin auftreten Update das Skript Manuell über GitHub.")
+                return end
+                local f = io.open(filesystem.scripts_dir()..SCRIPT_RELPATH, "wb")
+                f:write(a)
+                f:close()
+                util.toast("Athego‘s Skript wurde erfolgreich Aktualisiert. Bitte starte das Skript neu :)")
+                util.stop_script()
+            end)
+            async_http.dispatch()
+        end)
+    end
+end, function() response = true end)
+async_http.dispatch()
+repeat 
+    util.yield()
+until response
+
 all_players = {}
 
 local createPed = PED.CREATE_PED
